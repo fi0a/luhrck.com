3548349bd41377ed5331591db2e8496321b1835d
FIDot <fid@luhrck.com>
2024-02-07 18:26:32 +0000

    fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <a className={anchorStyle} href="https://don.luhrck.com/@fid">mastodon: @fid</a><br />
           <a className={anchorStyle} href="https://bsky.app/profile/luhrck.com">Bluesky: luhrck.com</a><br />
           <a className={anchorStyle} href="https://concurrent.world/entity/CC506dDf8C7d5B0ea9242C699F628756E674E8Af26">Concurrent: CC50...</a><br />
-          <a className={anchorStyle} href="https://fluffy.social/@fid">Misskey(fluffy.social): luhrck.com</a><br />
+          <a className={anchorStyle} href="https://fluffy.social/@fid">Misskey(fluffy.social): @fid</a><br />
           <a className={anchorStyle} href="https://twitter.com/Fi9c">Twitter: @Fi9c</a><br />
           <a className={anchorStyle} href="https://twitter.com/Fi0a">Twitter: @Fi0a</a><br />
           <a className={anchorStyle} href="https://pixiv.me/filiarc">pixiv: filiarc</a><br />

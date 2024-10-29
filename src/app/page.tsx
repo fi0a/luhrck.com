a63e2bb2ef0c36b99727150d5a94a5e7ea882f27
FIDot <fid@luhrck.com>
2024-10-29 20:51:58 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,12 @@ export default function Home() {
         <h1>Luhrck.com</h1>
 
         <div className="flex flex-col gap-1">
-          <a className={anchorStyle} href="https://don.luhrck.com/@fid">mastodon: @fid</a><br />
+          <a className={anchorStyle} href="https://pixiv.me/filiarc">pixiv: filiarc</a><br />
+          <a className={anchorStyle} href="https://concrnt.world/luhrck.com">Concrnt: @luhrck.com</a><br />
           <a className={anchorStyle} href="https://bsky.app/profile/luhrck.com">Bluesky: luhrck.com</a><br />
-          <a className={anchorStyle} href="https://concurrent.world/entity/CC506dDf8C7d5B0ea9242C699F628756E674E8Af26">Concurrent: CC50...</a><br />
           <a className={anchorStyle} href="https://fluffy.social/@fid">Misskey(fluffy.social): @fid</a><br />
           <a className={anchorStyle} href="https://twitter.com/Fi9c">Twitter: @Fi9c</a><br />
           <a className={anchorStyle} href="https://twitter.com/Fi0a">Twitter: @Fi0a</a><br />
-          <a className={anchorStyle} href="https://pixiv.me/filiarc">pixiv: filiarc</a><br />
         </div>
       </div>
     </main>
